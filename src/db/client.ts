1734411d9671a66ee97de42b8296ec7682976fb5
NGR-Np <ngr112001@gmail.com>
2026-02-09 01:59:40 +0545

    testing 2

--- src/db/client.ts
+++ src/db/client.ts
@@ -2,5 +2,7 @@ import { createClient, SupabaseClient } from "@supabase/supabase-js";
 import type { Env } from "../env";
 
 export function getSupabase(env: Env): SupabaseClient {
+  console.log(env.SUPABASE_URL)
   return createClient(env.SUPABASE_URL, env.SUPABASE_SERVICE_ROLE_KEY);
 }
+te
